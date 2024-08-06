b95f1afb3d24c8b57c7edec727156cf55efcd423
MaritzaFSilva <maritzafer98@gmail.com>
2024-08-06 09:13:00 -0300

    fix: ajustes de aula 06/08

--- src/main/java/maritza/ifpr/App.java
+++ src/main/java/maritza/ifpr/App.java
@@ -1,5 +1,7 @@
 package maritza.ifpr;
 
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.geometry.Pos;
@@ -14,18 +16,73 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+
+    private ControladorQuiz controladorQuiz;
+    // @Override
+    // public void init() throws Exception {
+    // super.init();
+
+    // ArrayList<Questao> lista = new ArrayList<>();
+
+    // lista.add(new Questao("Qual a capital do Paraná?", "Curitiba", new
+    // String[]{"Floripa", "Porto Alegre", "São Paulo"}));
+    // lista.add(new Questao("Qual a capital de São Paulo?", "São Paulo", new
+    // String[]{"Floripa", "Rio de Janeiro", "Campo Grande"}));
+    // lista.add(new Questao("Qual a capital do Acre?", "Rio Branco", new
+    // String[]{"Manaus","Palmas", "João Pessoa"}));
+    // lista.add(new Questao("Qual a capital de Rondonia?", "Porto Velho", new
+    // String[]{"Belém", "Macapá", "Manaus"}));
+
+    // }
+
+    @Override
+    public void init() throws Exception {
+
+        ArrayList<Questao> lista = new ArrayList<>();
+
+        lista.add(new Questao("Qual a cor favorita da prof?", "rosa",
+                new String[] { "preto", "laranja", "roxo", "vermelho" }));
+        lista.add(new Questao("Qual a cor favorita da prof 2?", "verde",
+                new String[] { "preto", "laranja", "roxo", "vermelho" }));
+        lista.add(new Questao("Qual a cor favorita da prof? 3", "azul",
+                new String[] { "preto", "laranja", "roxo", "vermelho" }));
+        lista.add(new Questao("Qual a cor favorita da prof? 4", "cinza",
+                new String[] { "preto", "laranja", "roxo", "vermelho" }));
+        lista.add(new Questao("Qual a cor favorita da prof? 5", "marrom",
+                new String[] { "preto", "laranja", "roxo", "vermelho" }));
+
+        controladorQuiz = new ControladorQuiz(lista);
+        
+    }
+
+ 
+
     @Override
     public void start(Stage stage) throws Exception {
+
         VBox root = new VBox();
         // root.setStyle("-fx-background-color: green");
 
-        Text enunciado = new Text("Enunciado");
+        Questao questao = this.controladorQuiz.getQuestao();
+    
+
+        Text enunciado = new Text(questao.getEnunciado());
         root.getChildren().add(enunciado);
         root.setAlignment(Pos.CENTER);
         root.setSpacing(10.0);
 
-        Button alternativa1 = new Button("Alternativa 1");
+
+        
+        Button alternativa1 = new Button(questao.getTodasAlternativas().get(0));
         root.getChildren().add(alternativa1);
+        Button alternativa2 = new Button(questao.getTodasAlternativas().get(1));
+        root.getChildren().add(alternativa2);
+        Button alternativa3 = new Button(questao.getTodasAlternativas().get(2));
+        root.getChildren().add(alternativa3);
+        Button alternativa4 = new Button("Alternativa 4");
+        root.getChildren().add(alternativa4);
+        Button alternativa5 = new Button("Alternativa 5");
+        root.getChildren().add(alternativa5);
 
         alternativa1.setOnAction(this::respondeQuestao);
 
